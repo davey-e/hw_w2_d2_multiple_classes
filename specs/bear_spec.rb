51d388c37da2165750b1521cc85c3484c2e83ea8
Dave Ellis <dave@ellisnetmail.co.uk>
2017-11-29 10:24:08 +0000

    Added comments and code changes from homework review

--- specs/bear_spec.rb
+++ specs/bear_spec.rb
@@ -4,12 +4,17 @@ require_relative("../bear")
 require_relative("../river")
 require_relative("../fish")
 
+#If you need to create an instance of an object (e.g. in setup) then you need to require_relative
+
 class TestBear < MiniTest::Test
 
   def setup()
     @yogi = Bear.new("Yogi")
-    @amazon = River.new("Amazon", ["Salmon1", "Salmon2", "Trout1", "Trout2"])
     @salmon1 = Fish.new("Salmon1")
+    @salmon2 = Fish.new("Salmon2")
+    @trout1 = Fish.new("Trout2")
+    @trout2 = Fish.new("Trout2")
+    @amazon = River.new("Amazon", [@salmon1, @salmon2, @trout1, @trout2])
   end
 
   def test_has_name
--- specs/river_spec.rb
+++ specs/river_spec.rb
@@ -3,15 +3,23 @@ require("minitest/rg")
 require_relative("../river")
 require_relative("../fish")
 
+#If you need to create an instance of an object (e.g. in setup) then you need to require_relative
+
 
 class TestRiver < MiniTest::Test
 
   def setup()
+    @salmon1 = Fish.new("Salmon1")
+    @salmon2 = Fish.new("Salmon2")
     @salmon3 = Fish.new("Salmon3")
     @salmon4 = Fish.new("Salmon4")
+    @trout1 = Fish.new("Trout1")
+    @trout2 = Fish.new("Trout2")
     @trout3 = Fish.new("Trout3")
     @trout4 = Fish.new("Trout4")
-    @amazon = River.new("Amazon", ["Salmon1", "Salmon2", "Trout1", "Trout2"])
+    @fish_array = [@salmon1, @salmon2, @trout1, @trout2]
+    @amazon = River.new("Amazon", @fish_array)
+    #@amazon = River.new("Amazon", ["Salmon1", "Salmon2", "Trout1", "Trout2"])
   end
 
   def test_has_name
@@ -21,7 +29,8 @@ class TestRiver < MiniTest::Test
   end
 
   def test_has_fish
-    expected = ["Salmon1", "Salmon2", "Trout1", "Trout2"]
+    expected = [@salmon1, @salmon2, @trout1, @trout2]
+    #expected = ["Salmon1", "Salmon2", "Trout1", "Trout2"]
     actual = @amazon.fish
     assert_equal(expected, actual)
   end
